4b7aff75d303fda11bdb1627a83e20bbbea4841e
lzjustc <ustclzj@foxmail.com>
2018-05-31 00:11:47 +0800

    anno

--- bots/twitter/manage.py
+++ bots/twitter/manage.py
@@ -1,10 +1,5 @@
 import sys
-from client import get_clients
-
-
-def test(*args):
-    print(test)
-    print(args)
+from .client import get_clients
 
 
 def like(*args):
@@ -13,10 +8,9 @@ def like(*args):
     """
     if len(args) == 0:
         return
-
+    # 构建回调函数
     def func(client):
         client.like(screen_name=args[0])
-
     invoke(func, args)
 
 
@@ -27,19 +21,19 @@ def send(*args):
     if len(args) == 0:
         return
     message = args[0]
-
     def func(client):
         client.send_message(message)
-
     invoke(func, args)
 
 
 def invoke(func, args):
     clients = get_clients()
     if len(args) == 1:
+    # 只有一个参数，对所有client都回调函数
         for client in clients:
             func(client)
     else:
+    # 寻找-{attr}={value} 的参数，对符合条件的client调用
         for arg in args[1:]:
             if arg[0] != '-':
                 continue
@@ -52,12 +46,14 @@ def invoke(func, args):
 
 
 def main():
+    """
+	命令行参数，调用名为argv[1]的函数，其他argvs作为参数传入
+    """
     argv = sys.argv
     if len(argv) < 2:
         pass
     else:
-        action = argv[1]
-        globals()[action](*argv[2:])
+        globals()[argv[1](*argv[2:])
 
 
 if __name__ == '__main__':
